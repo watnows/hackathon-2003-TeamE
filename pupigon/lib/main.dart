8b8bc048c6260a9f1550d9995aedf0532734e30f
shivasaki <shivasaki0329@gmail.com>
2023-09-17 15:58:09 +0900

    add: web firebase config

--- pupigon/lib/main.dart
+++ pupigon/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pupigon/firebase_options.dart';
 import 'package:pupigon/home_page.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Pupigon());
 }
 
